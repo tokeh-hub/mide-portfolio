bece628b29eeec7b953dbcc9e1040c40b4842427
tokeh-hub <olatunjiaisha12@gmail.com>
2021-12-14 22:48:11 +0100

    added target=blank

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -21,7 +21,7 @@ export const Portfolio = () => {
                                <div className='card-back'>
                                      <h3>{title}</h3>
                                     
-                                         <a className='arrow' href={url}>{icon}</a>
+                                         <a className='arrow' target="_blank"  href={url}>{icon}</a>
                                 
                                </div>
                            {/* </div> */}
