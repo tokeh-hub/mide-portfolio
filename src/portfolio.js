c0dd997a4cb81b3bfbb100eba68acec56c0bf0a9
tokeh-hub <olatunjiaisha12@gmail.com>
2022-01-23 20:41:52 +0100

    worked on toggle positioning

--- src/portfolio.js
+++ src/portfolio.js
@@ -5,73 +5,73 @@ import { FiArrowRightCircle } from "react-icons/fi";
 export const portfolio = [
     {
         id:1,
-        img:'./bg-generator.png',
-        title:'Background Generator',
-        url:'https://background-generator-mi.netlify.app/',
+        img:'./bmi.png',
+        title:'Adult BMI Checker',
+        url:'https://mide-bmi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
         id:2,
-        img:'./bbad.png',
-        title:'Breaking Bad Cast',
-        url:'https://breakbad-mi.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:3,
-        img:'./grocery.png',
-        title:'Grocery List',
-        url:'https://grocery-bud-mi.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:4,
-        img:'./splitter.png',
-        title:'Bill Splitter',
-        url:'https://tip-calculator-mi.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:5,
-        img:'./stripe.png',
-        title:'Stripe Landing Page',
-        url:'https://stripe-mi.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    
-    {
-        id:6,
-        img:'./fylo.png',
-        title:'Fylo Landing Page',
-        url:'https://toke.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:7,
-        img:'./lorem.png',
-        title:'Text Generator',
-        url:'https://text-generator-mi.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:8,
-        img:'./phone-verify.png',
-        title:'Phone Validator',
-        url:'https://phoneverify-mi.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:9,
         img:'./currency.png',
         title:'Currency Converter',
         url:'https://currencyconvertr.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:10,
+        id:3,
+        img:'./splitter.png',
+        title:'Bill Splitter',
+        url:'https://tip-calculator-mi.netlify.app/',
+        icon: <FiArrowRightCircle/>
+    },
+    {
+        id:4,
+        img:'./grocery.png',
+        title:'Grocery List',
+        url:'https://grocery-bud-mi.netlify.app/',
+        icon: <FiArrowRightCircle/>
+    },
+    {
+        id:5,
         img:'./mitodo.png',
         title:'My Todo',
         url:'https://midetodo.netlify.app/',
+        icon: <FiArrowRightCircle/>
+    },
+    {
+        id:6,
+        img:'./bbad.png',
+        title:'Breaking Bad Cast',
+        url:'https://breakbad-mi.netlify.app/',
+        icon: <FiArrowRightCircle/>
+    },
+    
+    {
+        id:7,
+        img:'./fylo.png',
+        title:'Fylo Landing Page',
+        url:'https://toke.netlify.app/',
+        icon: <FiArrowRightCircle/>
+    },
+    {
+        id:8,
+        img:'./stripe.png',
+        title:'Stripe Landing Page',
+        url:'https://stripe-mi.netlify.app/',
+        icon: <FiArrowRightCircle/>
+    },
+    {
+        id:9,
+        img:'./lorem.png',
+        title:'Text Generator',
+        url:'https://text-generator-mi.netlify.app/',
+        icon: <FiArrowRightCircle/>
+    },
+    {
+        id:10,
+        img:'./phone-verify.png',
+        title:'Phone Validator',
+        url:'https://phoneverify-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
@@ -80,12 +80,6 @@ export const portfolio = [
         title:'Rock Paper Scissors',
         url:'https://quirky-meitner-f05e1a.netlify.app/',
         icon: <FiArrowRightCircle/>
-    },
-    {
-        id:12,
-        img:'./bmi.png',
-        title:'Adult BMI Checker',
-        url:'https://mide-bmi.netlify.app/',
-        icon: <FiArrowRightCircle/>
     }
+    
 ]
