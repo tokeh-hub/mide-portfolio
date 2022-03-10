86fdf4a4f766f36cb959135faa68be3d4be4cb56
tokeh-hub <olatunjiaisha12@gmail.com>
2022-03-10 23:52:14 +0100

    commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -18,13 +18,15 @@ export const portfolio = [
         url:'https://cryptoverse-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
+    
     {
         id:3,
-        img:'./sunny.png',
-        title:'SunnySide Landing Page',
-        url:'https://sunnyside-mi.netlify.app/',
+        img:'./rps.png',
+        title:'Rock Paper Scissors',
+        url:'https://quirky-meitner-f05e1a.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
+    
     {
         id:4,
         img:'./travel.png',
@@ -34,20 +36,18 @@ export const portfolio = [
     },
     {
         id:5,
+        img:'./sunny.png',
+        title:'SunnySide Landing Page',
+        url:'https://sunnyside-mi.netlify.app/',
+        icon: <FiArrowRightCircle/>
+    },
+    {
+        id:6,
         img:'./bmi.png',
         title:'Adult BMI Checker',
         url:'https://mide-bmi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
-    
-    {
-        id:6,
-        img:'./splitter.png',
-        title:'Bill Splitter',
-        url:'https://tip-calculator-mi.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    
     {
         id:7,
         img:'./bbad.png',
@@ -55,7 +55,6 @@ export const portfolio = [
         url:'https://breakbad-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
-    
     {
         id:8,
         img:'./fylo.png',
@@ -63,14 +62,17 @@ export const portfolio = [
         url:'https://toke.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
-    
-   
     {
         id:9,
-        img:'./rps.png',
-        title:'Rock Paper Scissors',
-        url:'https://quirky-meitner-f05e1a.netlify.app/',
+        img:'./splitter.png',
+        title:'Bill Splitter',
+        url:'https://tip-calculator-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     }
+    
+    
+    
+    
+   
     
 ]
