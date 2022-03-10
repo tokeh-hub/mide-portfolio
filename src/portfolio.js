7de00c7b3fbb383eb9208152ecc515a44343cb9a
tokeh-hub <olatunjiaisha12@gmail.com>
2022-03-10 23:41:12 +0100

    deleted some projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -39,36 +39,17 @@ export const portfolio = [
         url:'https://mide-bmi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
+    
     {
         id:6,
-        img:'./currency.png',
-        title:'Currency Converter',
-        url:'https://currencyconvertr.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:7,
         img:'./splitter.png',
         title:'Bill Splitter',
         url:'https://tip-calculator-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
+    
     {
-        id:8,
-        img:'./grocery.png',
-        title:'Grocery List',
-        url:'https://grocery-bud-mi.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:9,
-        img:'./mitodo.png',
-        title:'My Todo',
-        url:'https://midetodo.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:10,
+        id:7,
         img:'./bbad.png',
         title:'Breaking Bad Cast',
         url:'https://breakbad-mi.netlify.app/',
@@ -76,35 +57,16 @@ export const portfolio = [
     },
     
     {
-        id:11,
+        id:8,
         img:'./fylo.png',
         title:'Fylo Landing Page',
         url:'https://toke.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
+    
+   
     {
-        id:12,
-        img:'./stripe.png',
-        title:'Stripe Landing Page',
-        url:'https://stripe-mi.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:13,
-        img:'./lorem.png',
-        title:'Text Generator',
-        url:'https://text-generator-mi.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:14,
-        img:'./phone-verify.png',
-        title:'Phone Validator',
-        url:'https://phoneverify-mi.netlify.app/',
-        icon: <FiArrowRightCircle/>
-    },
-    {
-        id:15,
+        id:9,
         img:'./rps.png',
         title:'Rock Paper Scissors',
         url:'https://quirky-meitner-f05e1a.netlify.app/',
