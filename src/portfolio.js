1101f0b3c5e57a7032f422d06b7040e4a53e4128
tokeh-hub <olatunjiaisha12@gmail.com>
2022-03-10 23:25:52 +0100

    added countries app

--- src/portfolio.js
+++ src/portfolio.js
@@ -5,62 +5,70 @@ import { FiArrowRightCircle } from "react-icons/fi";
 export const portfolio = [
     {
         id:1,
+        img:'./countries.png',
+        title:'Country Finder with RESTful API',
+        url:'https://countriesapi-mi.netlify.app/',
+        icon: <FiArrowRightCircle/>
+    },
+
+    {
+        id:2,
         img:'./crypto.png',
         title:'Cryptoverse',
         url:'https://cryptoverse-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:2,
+        id:3,
         img:'./sunny.png',
         title:'SunnySide Landing Page',
         url:'https://sunnyside-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:3,
+        id:4,
         img:'./travel.png',
         title:'Travel Advisor',
         url:'https://travel-advisor-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:4,
+        id:5,
         img:'./bmi.png',
         title:'Adult BMI Checker',
         url:'https://mide-bmi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:5,
+        id:6,
         img:'./currency.png',
         title:'Currency Converter',
         url:'https://currencyconvertr.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:6,
+        id:7,
         img:'./splitter.png',
         title:'Bill Splitter',
         url:'https://tip-calculator-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:7,
+        id:8,
         img:'./grocery.png',
         title:'Grocery List',
         url:'https://grocery-bud-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:8,
+        id:9,
         img:'./mitodo.png',
         title:'My Todo',
         url:'https://midetodo.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:9,
+        id:10,
         img:'./bbad.png',
         title:'Breaking Bad Cast',
         url:'https://breakbad-mi.netlify.app/',
@@ -68,35 +76,35 @@ export const portfolio = [
     },
     
     {
-        id:10,
+        id:11,
         img:'./fylo.png',
         title:'Fylo Landing Page',
         url:'https://toke.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:11,
+        id:12,
         img:'./stripe.png',
         title:'Stripe Landing Page',
         url:'https://stripe-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:12,
+        id:13,
         img:'./lorem.png',
         title:'Text Generator',
         url:'https://text-generator-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:13,
+        id:14,
         img:'./phone-verify.png',
         title:'Phone Validator',
         url:'https://phoneverify-mi.netlify.app/',
         icon: <FiArrowRightCircle/>
     },
     {
-        id:14,
+        id:15,
         img:'./rps.png',
         title:'Rock Paper Scissors',
         url:'https://quirky-meitner-f05e1a.netlify.app/',
